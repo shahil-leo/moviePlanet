21bab12f76caec5fc6556a44eb08f07dae3b76cf
shahil <shahilcode@gmail.com>
2023-04-23 21:01:50 +0530

    added the the slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,29 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { DetailsComponent } from './components/details/details.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component'
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SideBarComponent
+    SideBarComponent,
+    DetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
